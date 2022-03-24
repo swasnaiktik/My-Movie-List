2648282416e25c4a0349a5463fdcda4c8c457ca3
Swastik Naik <swasnaiktik@gmail.com>
2022-03-23 20:56:07 -0400

    Working load sql file

--- createLoad.py
+++ createLoad.py
@@ -1,18 +1,28 @@
 import csv
 
-insert_Genre = {}
 insert_Star = {}
 insert_Director = {}
 insert_Certificate = {}
 
+def getQuery(table, column, value):
+    value = value.replace("'", "''")
+    return f"INSERT INTO {table}({column}) VALUES ('{value}');\n"
 
-
-f'INSERT INTO Genre(name) VALUES ({})'
-with open('./load.sql', encoding="utf-8") as loadFile:
+with open('./load.sql', 'a', encoding='utf-8') as loadFile:
 
     with open('./Data/imdb_top_1000.csv', encoding="utf-8") as csvfile:
         reader = csv.DictReader(csvfile)
-
         for row in reader:
-            print(row)
-            
+            if (not insert_Certificate.get(row['Certificate'], False)) and row['Certificate'] != '' :
+                insert_Director[row['Certificate']] = True
+                loadFile.write(getQuery('Certificate', 'category', row['Certificate']))            
+
+            if (not insert_Director.get(row['Director'], False)) and row['Director'] != '' :
+                insert_Director[row['Director']] = True
+                loadFile.write(getQuery('Director', 'name', row['Director']))            
+
+            for i in range(1,5):
+                i = str(i)
+                if (not insert_Star.get(row['Star'+i], False)) and row['Star'+i] != '' :
+                    insert_Star[row['Star'+i]] = True
+                    loadFile.write(getQuery('Star', 'name', row['Star'+i]))
