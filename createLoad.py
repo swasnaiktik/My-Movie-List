8f683db299c3f4ac14027073681a25e56ba55c3d
Swastik Naik <swasnaiktik@gmail.com>
2022-03-26 16:30:33 -0400

    Added Movie_Genre query function

--- createLoad.py
+++ createLoad.py
@@ -80,32 +80,49 @@ def getIMDBQuery(row):
 
     return query
 
-with open('./load.sql', 'a', encoding='utf-8') as loadFile:
+def getMovieGenreQuery(row, genre):
 
-    with open('./Data/imdb_top_1000.csv', encoding="utf-8") as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            if (not insert_Certificate.get(row['Certificate'], False)) and row['Certificate'] != '' :
-                insert_Certificate[row['Certificate']] = True
-                loadFile.write(getQuery('Certificate', 'category', row['Certificate']))            
+    query = f"INSERT INTO Movie_Genre(id_movie, id_genre)\n\
+            SELECT m.id, g.id FROM IMDB m, Genre g WHERE\n\
+            m.title = {row['Series_Title']} AND g.name = {genre};\n"
+    
+    return query
 
-            if (not insert_Director.get(row['Director'], False)) and row['Director'] != '' :
-                insert_Director[row['Director']] = True
-                loadFile.write(getQuery('Director', 'name', row['Director']))            
+if __name__ == '__main__':
 
-            for i in range(1,5):
-                i = str(i)
-                if (not insert_Star.get(row['Star'+i], False)) and row['Star'+i] != '' :
-                    insert_Star[row['Star'+i]] = True
-                    loadFile.write(getQuery('Star', 'name', row['Star'+i]))
-            
-            if row['Genre'] != '': 
-                genre_list = row['Genre'].split(',')
+    with open('./load.sql', 'a', encoding='utf-8') as loadFile:
+
+        with open('./Data/imdb_top_1000.csv', encoding="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if (not insert_Certificate.get(row['Certificate'], False)) and row['Certificate'] != '' :
+                    insert_Certificate[row['Certificate']] = True
+                    loadFile.write(getQuery('Certificate', 'category', row['Certificate']))            
+
+                if (not insert_Director.get(row['Director'], False)) and row['Director'] != '' :
+                    insert_Director[row['Director']] = True
+                    loadFile.write(getQuery('Director', 'name', row['Director']))            
+
+                for i in range(1,5):
+                    i = str(i)
+                    if (not insert_Star.get(row['Star'+i], False)) and row['Star'+i] != '' :
+                        insert_Star[row['Star'+i]] = True
+                        loadFile.write(getQuery('Star', 'name', row['Star'+i]))
+                
+                genre_list = []
+
+                if row['Genre'] != '': 
+                    genre_list+= row['Genre'].split(',')
+                    for genre in genre_list:
+                        genre = genre.strip()
+                        if not insert_Genre.get(genre, False):
+                            insert_Genre[genre] = True
+                            loadFile.write(getQuery('Genre', 'name', genre))
+                
+                loadFile.write(getIMDBQuery(row))
+
                 for genre in genre_list:
                     genre = genre.strip()
-                    if not insert_Genre.get(genre, False):
-                        insert_Genre[genre] = True
-                        loadFile.write(getQuery('Genre', 'name', genre))
-            
-            loadFile.write(getIMDBQuery(row))
+                    loadFile.write(getMovieGenreQuery(row, genre))
+
 
